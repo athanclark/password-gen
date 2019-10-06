77f49931e1ff564c467fa33fbcc83a3a41a9739a
Athan Clark <athan.clark@gmail.com>
2019-10-06 13:01:33 -0700

    operating executable

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,33 @@
 module Main where
 
 import Lib
+import Options.Applicative
+
 
 main :: IO ()
-main = someFunc
+main = do
+  (salt,subject) <- execParser opts
+  putStrLn (makePassword salt subject)
+  where
+    opts = info (pwOptions <**> helper)
+        ( fullDesc
+      <> progDesc "Generate a password via SCrypt, Base58 encoded for SUBJECT over SALT"
+      <> header "password-gen - a password generator"
+        )
+
+
+
+pwOptions :: Parser (String,String)
+pwOptions = (,) <$> salt <*> subject
+  where
+    salt = strOption
+        ( long "salt"
+      <> short 's'
+      <> metavar "SALT"
+        )
+
+    subject = strOption
+        ( long "subject"
+      <> short 'p'
+      <> metavar "SUBJECT"
+        )
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,24 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Crypto.KDF.Scrypt (Parameters (..), generate)
+import Data.ByteString (ByteString)
+import Data.ByteString.Base58 (bitcoinAlphabet, encodeBase58)
+import Data.ByteString.UTF8 (fromString, toString)
+
+
+params :: Parameters
+params = Parameters
+  { n = 16384
+  , r = 16
+  , p = 1
+  , outputLength = 32
+  }
+
+
+makePassword :: String -- ^ Salt
+             -> String -- ^ Subject
+             -> String
+makePassword salt subject = toString (encodeBase58 bitcoinAlphabet password)
+  where
+    password :: ByteString
+    password = generate params (fromString subject) (fromString salt)
